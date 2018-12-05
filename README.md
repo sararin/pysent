cb7e91834a5ca5813f55c699909131edac8596a3
sararin <sararin@cock.li>
2018-12-05 16:49:02 +0100

    i do hate myself :)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pysent
 shameless bloated clone of suckless' sent program
+
+and i know this
+is heresy
+
+i don't care
+do you know?
+
+
+because i am lazy
+piece of shit, you
+fuckers
--- pysent.py
+++ pysent.py
@@ -2,7 +2,7 @@ import sys #ugly boilerplate
 if sys.version_info[0] == 2:
   import Tkinter
 else:
-  import tkinter
+  from tkinter import *
 
 class Reader: # needs tighter integration and dynamic loading,
               # shouldn't store entire presentation at once
@@ -25,11 +25,28 @@ class Reader: # needs tighter integration and dynamic loading,
     self.presentation.append(self._send_current_page())
     return self.presentation
 
+
+def right_key(event):
+    global num
+    if num < (len(slides) - 1):
+      num = num + 1
+    w.config(text=''.join(slides[num]))
+
+def left_key(event):
+    global num
+    if num > 0:
+      num = num - 1
+    w.config(text=''.join(slides[num]))
+
 if __name__ == "__main__":
+  num = 0
   script, filename = sys.argv[0], sys.argv[1]
   reader = Reader(filename)
   slides = reader.create_presentation()
-  # root = tkinter.Tk()
-  # w = tkinter.Label(root, text="Hello")
-  # w.pack()
-  # root.tkinter.mainloop()
+
+  root = Tk()
+  root.bind('<Right>', right_key)
+  root.bind('<Left>', left_key)
+  w = Label(root, text=''.join(slides[num]))
+  w.pack(side=TOP, expand=YES, fill=BOTH)
+  root.mainloop()
